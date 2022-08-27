894f38da983f6235970d364d62965af149dcb48c
Balint <privatebalint@gmail.com>
2022-08-27 11:08:00 +0200

    feat: Implement basic port scanning

--- scanner.py
+++ scanner.py
@@ -17,3 +17,21 @@ print("-" * 50)
 print("Scanning target " + target)
 print("Time started: " + str(datetime.now()))
 print("-" * 50)
+
+try:
+    for port in range(50, 85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print(f'Port {format(port)} is open')
+        s.close()
+except KeyboardInterrupt:
+    print("\nInterrupted")
+    sys.exit()
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+except socket.error:
+    print("Couldn't connect to server.")
+    sys.exit()
